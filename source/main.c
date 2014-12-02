e4cf46c2f55441ae1d734b3c8f9d617ceff7d577
ThetaSigma <thete@SatelliteFive.princeton.edu>
2014-12-02 13:24:34 -0800

    initialising from xem's 3DShomebrew template

---
+++ source/main.c
@@ -0,0 +1,50 @@
+#include <3ds.h>
+
+int main() //using xem's template
+{
+  // Initializations
+  srvInit();        // services
+  aptInit();        // applets
+  hidInit(NULL);    // input
+  gfxInit();        // graphics
+  gfxSet3D(true);  // stereoscopy (true: enabled / false: disabled)
+  u32 kDown;        // keys down
+  u32 kHeld;        // keys pressed
+  u32 kUp;          // keys up
+
+  // Main loop
+  while (aptMainLoop())
+  {
+
+    // Wait for next frame
+    gspWaitForVBlank();
+
+    // Read which buttons are currently pressed or not
+    hidScanInput();
+    kDown = hidKeysDown();
+    kHeld = hidKeysHeld();
+    kUp = hidKeysUp();
+
+    // If START button is pressed, break loop and quit
+    if (kDown & KEY_START){
+      break;
+    }
+
+
+    /** Your code goes here **/
+
+
+    // Flush and swap framebuffers
+    gfxFlushBuffers();
+    gfxSwapBuffers();
+  }
+
+  // Exit
+  gfxExit();
+  hidExit();
+  aptExit();
+  srvExit();
+
+  // Return to hbmenu
+  return 0;
+}
