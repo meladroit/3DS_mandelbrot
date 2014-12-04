cc1ad7736dcf82bd5f0da6170afb149b46465a8a
meladroit <meladroit@users.noreply.github.com>
2014-12-04 01:42:00 -0800

    it works\!

--- source/main.c
+++ source/main.c
@@ -1,124 +1,106 @@
 #include <3ds.h>
+#include <string.h>
 // #include <math.h>
 
 #define XLOWER -2.375
 #define XUPPER 1.375
 #define YLOWER -1.125
 #define YUPPER 1.125
-#define SCALING 0.006875
+#define SCALING 0.009375
 #define MAX_ITER 42
 #define APPROXIMATELY_INFINITY 1.0e5
 
 #define WIDTH_TOP 400
 #define WIDTH_BOTTOM 320
+#define WIDTH_DIFF (WIDTH_TOP-WIDTH_BOTTOM)/2
 #define HEIGHT 240
 
 #define CONFIG_3D_SLIDERSTATE (*(float*)0x1FF81080) //thanks shinyquagsire23
-#define MAX_STEREO_SEPARATION 10
+#define MAX_STEREO_SEPARATION 42
 #define STEREO_SEPARATION (MAX_STEREO_SEPARATION * CONFIG_3D_SLIDERSTATE)
 
-void draw_mandelbrot()
+void calc_mandelbrot()
 {
     //implementation of Wikipedia's escape time pseudocode
     int i,j,k;
+    int sep = (int)STEREO_SEPARATION/2;
     u32 idx;
-    bool escaped[WIDTH_TOP*HEIGHT];
-    float x1[WIDTH_TOP*HEIGHT];
-    float y1[WIDTH_TOP*HEIGHT];
     float x0;
     float y0;
     float x,y;
     float xtemp;
-    float px_iteration;
-    int stereo_adj;
-    u16 fbWidth, fbHeight;
+    float iter_frac;
 
-    u8* fbAdr_L = gfxGetFramebuffer(GFX_TOP,GFX_LEFT,&fbWidth,&fbHeight);
-    //u8* fbAdr_R = gfxGetFramebuffer(GFX_TOP,GFX_RIGHT,&fbWidth,&fbHeight);
-
-    for (k=0;k<MAX_ITER;k++)
+    u8* fbAdr_L = gfxGetFramebuffer(GFX_TOP,GFX_LEFT,NULL,NULL);
+    u8* fbAdr_R = gfxGetFramebuffer(GFX_TOP,GFX_RIGHT,NULL,NULL);
+    u8* fbAdr_B = gfxGetFramebuffer(GFX_BOTTOM,GFX_LEFT,NULL,NULL);
+    memset(fbAdr_L,0,WIDTH_TOP*HEIGHT*3);
+    memset(fbAdr_R,0,WIDTH_TOP*HEIGHT*3);
+    for (i=0;i<WIDTH_TOP;i++)
     {
-        for (i=0;i<WIDTH_TOP;i++)
+        x0 = XLOWER+i*SCALING;
+        for (j=0;j<HEIGHT;j++)
         {
-            x0 = XLOWER+i*SCALING;
-            for (j=0;j<HEIGHT;j++)
+            y0 = YLOWER+j*SCALING;
+            idx = i*HEIGHT+j;
+            x=0.0;
+            y=0.0;
+            for (k=0;k<MAX_ITER;k++)
             {
-                y0 = YLOWER+j*SCALING;
-                idx = i*HEIGHT+j;
-                if(0==k)
-                {
-                    escaped[idx]=false;
-                    x1[idx]=0.0;
-                    y1[idx]=0.0;
-                }
-                if(escaped[idx])
-                {
-                    continue;
-                }
-                x = x1[idx];
-                y = y1[idx];
                 xtemp = x*x-y*y+x0;
                 y = 2*x*y+y0;
                 x = xtemp;
-                x1[idx] = x;
-                y1[idx] = y;
                 if (x*x+y*y>APPROXIMATELY_INFINITY)
                 {
-                    escaped[idx]=true;
-                    px_iteration = 1.0;//(float)(k)/MAX_ITER;
-                    stereo_adj = 0;//(int)(px_iteration*STEREO_SEPARATION);
-
-                    if (i>=stereo_adj)
+                    iter_frac = (float)k/MAX_ITER;
+                    if ((i>=WIDTH_DIFF)&&(i<WIDTH_TOP-WIDTH_DIFF))
                     {
-                        idx = 3*(j+HEIGHT*i); //-stereo_adj);
-                        // u8* fbdL = &fbAdr_L[idx*3];
-                        fbAdr_L[idx] = 127;
-                        fbAdr_L[idx+1] = 0;
-                        fbAdr_L[idx+2] = 127;
-                        /* fbdL[1] = 0;
-                        fbdL[2] = 127;//(u8)(255*px_iteration); */
+                        fbAdr_B[idx*3-3*WIDTH_DIFF*HEIGHT] = (u8)(255*iter_frac);
                     }
-                    /*
-                    if (CONFIG_3D_SLIDERSTATE>0)
+                    if ((1-iter_frac)*sep<=i)
                     {
-                        i+= stereo_adj;
-                        if (i<WIDTH_TOP)
+                        idx-=(int)(HEIGHT*(1-iter_frac)*sep);
+                        if ((u8)(255*iter_frac)>fbAdr_L[idx*3])
                         {
-                            idx = j+HEIGHT*i;
-                            fbdR = &fbAdr_R[idx*3];
-                            fbdR[0] = (u8)(255*px_iteration);
-                            fbdR[1] = (u8)0;
-                            fbdR[2] = (u8)(255*px_iteration);
+                            fbAdr_L[idx*3] = (u8)(255*iter_frac);
                         }
                     }
-                    */
-                    continue;
+                    if (CONFIG_3D_SLIDERSTATE>0)
+                    {
+                        if ((1-iter_frac)*sep<WIDTH_TOP-i)
+                        {
+                            idx+=2*(int)(HEIGHT*(1-iter_frac)*sep);
+                            if ((u8)(255*iter_frac)>fbAdr_R[idx*3])
+                            {
+                                fbAdr_R[idx*3] = (u8)(255*iter_frac);
+                            }
+                        }
+                    }
+                    break;
+                }
+            }
+            if (k==MAX_ITER)
+            {
+                fbAdr_L[idx*3] = 255;
+                fbAdr_R[idx*3] = 255;
+                if ((i>=WIDTH_DIFF)&&(i<WIDTH_TOP-WIDTH_DIFF))
+                {
+                    fbAdr_B[idx*3-3*WIDTH_DIFF*HEIGHT] = 255;
                 }
             }
         }
     }
 }
 
-void fill_magenta()
+void draw_mandelbrot()
 {
-    u16 fbWidth, fbHeight;
-    int i,j,k,idx;
-    u8* fbAdr_L = gfxGetFramebuffer(GFX_TOP,GFX_LEFT,&fbWidth,&fbHeight);
-    u8* fbdL;
-    u8 fbdR[1]={255};
-    for (k=0;k<1;k++)
+    int i;
+    for (i=0;i<2;i++)
     {
-        for (i=0;i<WIDTH_TOP;i++)
-        {
-            for (j=0;j<HEIGHT;j++)
-            {
-                idx = i*HEIGHT+j;
-                fbdL = &fbAdr_L[idx*3];
-                fbdL[0]=fbdR[k];
-                fbdL[1]=0;
-                fbdL[2]=fbdR[k];
-            }
-        }
+        gspWaitForVBlank();
+        gfxFlushBuffers();
+        gfxSwapBuffers();
+        calc_mandelbrot();
     }
 }
 
@@ -133,7 +115,8 @@ int main() //using xem's template
   u32 kDown;        // keys down
   u32 kHeld;        // keys pressed
   u32 kUp;          // keys up
-  bool key_pressed = false;
+  float monitor_3d_slider = CONFIG_3D_SLIDERSTATE;
+  draw_mandelbrot();
 
   // Main loop
   while (aptMainLoop())
@@ -153,12 +136,8 @@ int main() //using xem's template
       break;
     }
 
-    if(!key_pressed)
-    {
-        fill_magenta();
-    }
-    if (kDown & KEY_A){
-        key_pressed = true;
+    if (CONFIG_3D_SLIDERSTATE!=monitor_3d_slider){
+        monitor_3d_slider = CONFIG_3D_SLIDERSTATE;
         draw_mandelbrot();
     }
 
