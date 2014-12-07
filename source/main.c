18e2059205b5b7b0c3e55b8b04f36a8760e18117
meladroit <meladroit@users.noreply.github.com>
2014-12-06 17:45:13 -0800

    semi-functional touch input; B to return to initial view

--- source/main.c
+++ source/main.c
@@ -1,6 +1,6 @@
 #include <3ds.h>
 #include <string.h>
-// #include <math.h>
+#include <stdlib.h>
 
 #define XLOWER -2.375
 #define XUPPER 1.375
@@ -8,6 +8,7 @@
 #define YUPPER 1.125
 #define SCALING 0.009375
 #define MAX_ITER 24
+#define ITER_DIFF 5
 #define APPROXIMATELY_INFINITY 1.0e5
 
 #define WIDTH_TOP 400
@@ -20,7 +21,22 @@
 #define MAX_STEREO_SEPARATION 42
 #define STEREO_SEPARATION (MAX_STEREO_SEPARATION * CONFIG_3D_SLIDERSTATE)
 
-void draw_mandelbrot(bool render_bottom)
+typedef struct touchHandler{
+    int rudimentaryTimer;
+    touchPosition touch1;
+    touchPosition touch2;
+} touchHandler;
+
+typedef struct Mandelbrot_params{
+    float xlower;
+    float xupper;
+    float ylower;
+    float yupper;
+    int zoomlevel;
+    int max_iter;
+} Mandelbrot_params;
+
+void draw_mandelbrot(Mandelbrot_params m, bool render_top, bool render_bottom)
 {
     //implementation of Wikipedia's escape time pseudocode
     int i,j,k;
@@ -29,6 +45,7 @@ void draw_mandelbrot(bool render_bottom)
     float y0;
     float x,y;
     float xtemp;
+    float scaling = SCALING/(int)(1<<m.zoomlevel);
     int sep = (int)STEREO_SEPARATION/2;
     float iter_frac;
     int sep_iter;
@@ -40,52 +57,58 @@ void draw_mandelbrot(bool render_bottom)
 
     for (i=0;i<WIDTH_TOP;i++)
     {
-        x0 = XLOWER+i*SCALING;
+        x0 = m.xlower+i*scaling;
         for (j=0;j<HEIGHT;j++)
         {
-            y0 = YLOWER+j*SCALING;
+            y0 = m.ylower+j*scaling;
             idx = (u32)i*HEIGHT+j;
             x=0.0;
             y=0.0;
-            for (k=0;k<MAX_ITER;k++)
+            for (k=0;k<m.max_iter;k++)
             {
                 xtemp = x*x-y*y+x0;
                 y = 2*x*y+y0;
                 x = xtemp;
                 if (x*x+y*y>APPROXIMATELY_INFINITY)
                 {
-                    iter_frac = (float)k/MAX_ITER;
+                    iter_frac = (float)k/m.max_iter;
                     sep_iter = (int)(sep-sep*iter_frac);
                     if (render_bottom && ((i>=WIDTH_DIFF)&&(i<WIDTH_TOP-WIDTH_DIFF)))
                     {
                         fbAdr_B[idx*3-3*WIDTH_DIFF*HEIGHT] = (u8)(255*iter_frac);
                     }
-                    if (sep_iter<=i)
+                    if (render_top)
                     {
-                        idx-=sep_iter*HEIGHT;
-                        if ((u8)(255*iter_frac)>fbAdr_L[idx*3])
+                        if (sep_iter<=i)
                         {
-                            fbAdr_L[idx*3] = (u8)(255*iter_frac);
-                        }
-                    }
-                    if (CONFIG_3D_SLIDERSTATE>0)
-                    {
-                        if (sep_iter<WIDTH_TOP-i)
-                        {
-                            idx+=2*sep_iter*HEIGHT;
-                            if ((u8)(255*iter_frac)>fbAdr_R[idx*3])
+                            idx-=sep_iter*HEIGHT;
+                            if ((u8)(255*iter_frac)>fbAdr_L[idx*3])
                             {
-                                fbAdr_R[idx*3] = (u8)(255*iter_frac);
+                                fbAdr_L[idx*3] = (u8)(255*iter_frac);
+                            }
+                        }
+                        if (CONFIG_3D_SLIDERSTATE>0)
+                        {
+                            if (sep_iter<WIDTH_TOP-i)
+                            {
+                                idx+=2*sep_iter*HEIGHT;
+                                if ((u8)(255*iter_frac)>fbAdr_R[idx*3])
+                                {
+                                    fbAdr_R[idx*3] = (u8)(255*iter_frac);
+                                }
                             }
                         }
                     }
                     break;
                 }
             }
-            if(k==MAX_ITER)
+            if(k==m.max_iter)
             {
-                fbAdr_L[idx*3] = 255;
-                fbAdr_R[idx*3] = 255;
+                if (render_top)
+                {
+                    fbAdr_L[idx*3] = 255;
+                    fbAdr_R[idx*3] = 255;
+                }
                 if (render_bottom && (i>=WIDTH_DIFF)&&(i<WIDTH_TOP-WIDTH_DIFF))
                 {
                     fbAdr_B[idx*3-3*WIDTH_DIFF*HEIGHT] = 255;
@@ -95,14 +118,44 @@ void draw_mandelbrot(bool render_bottom)
     }
 }
 
-void draw_mandelbrot_proper(bool render_bottom)
+void draw_mandelbrot_proper(Mandelbrot_params m, bool render_top, bool render_bottom)
 {
-    draw_mandelbrot(render_bottom);
-    //gfxFlushBuffers();
+    gfxFlushBuffers();
+    draw_mandelbrot(m, render_top,render_bottom);
     gfxSwapBuffers();
     gspWaitForVBlank();
-    draw_mandelbrot(render_bottom);
+    draw_mandelbrot(m, render_top,render_bottom);
     
+}
+
+void Mandelbrot_init(Mandelbrot_params* m)
+{
+    m->xlower = XLOWER;
+    m->ylower = YLOWER;
+    m->xupper = XUPPER;
+    m->yupper = YUPPER;
+    m->zoomlevel = 0;
+    m->max_iter = MAX_ITER;
+}
+
+bool handle_touch(touchHandler* t) // thanks smea and hbmenu people
+{
+    touchPosition touch;
+    hidTouchRead(&touch);
+    if(hidKeysDown()&KEY_TOUCH)
+	{
+		t->rudimentaryTimer=0;
+		t->touch1=touch;
+	}
+    else if((hidKeysUp() & KEY_TOUCH) && t->rudimentaryTimer<30 && abs(t->touch1.px-t->touch2.px)+abs(t->touch1.py-t->touch2.py)<12){
+		return true;
+	}
+    else if(hidKeysHeld() & KEY_TOUCH){
+		t->rudimentaryTimer++;
+	}
+    
+    t->touch2 = touch;	
+    return false;
 }
 
 int main() //using xem's template
@@ -113,12 +166,16 @@ int main() //using xem's template
   hidInit(NULL);    // input
   gfxInit();        // graphics
   gfxSet3D(true);   // stereoscopy (true: enabled / false: disabled)
-  u32 kDown;        // keys down
+  //u32 kDown;        // keys down
   //u32 kHeld;        // keys pressed
   //u32 kUp;          // keys up
 
   float monitor_3d_slider = CONFIG_3D_SLIDERSTATE;
-  draw_mandelbrot_proper(true);
+  float temp_scaling,temp_x,temp_y;
+  touchHandler t;
+  Mandelbrot_params m;
+  Mandelbrot_init(&m);
+  draw_mandelbrot_proper(m,true,true);
   
   // Main loop
   while (aptMainLoop())
@@ -129,18 +186,37 @@ int main() //using xem's template
 
     // Read which buttons are currently pressed or not
     hidScanInput();
-    kDown = hidKeysDown();
+    //kDown = hidKeysDown();
     //kHeld = hidKeysHeld();
     //kUp = hidKeysUp();
 
     // If START button is pressed, break loop and quit
-    if (kDown & KEY_START){
+    if (hidKeysDown() & KEY_START){
       break;
+    }
+    
+    if (handle_touch(&t))
+    {
+        temp_scaling = SCALING/(int)(1<<m.zoomlevel);
+        m.zoomlevel++;
+        temp_x = (t.touch2.px+WIDTH_BOTTOM/2-WIDTH_DIFF)*temp_scaling+m.xlower;
+        temp_y = (-t.touch2.py+HEIGHT+HEIGHT/4)*temp_scaling+m.ylower;
+        m.xlower = temp_x-temp_scaling*WIDTH_TOP/2;
+        m.xupper = temp_x+temp_scaling*WIDTH_TOP/2;
+        m.ylower = temp_y-temp_scaling*HEIGHT/2;
+        m.yupper = temp_y+temp_scaling*HEIGHT/2;
+        m.max_iter+=ITER_DIFF;
+        draw_mandelbrot_proper(m,true,true);        
+    }
+    
+    if (hidKeysDown() & KEY_B){
+      Mandelbrot_init(&m);
+      draw_mandelbrot_proper(m,true,true);
     }
     
     if (CONFIG_3D_SLIDERSTATE!=monitor_3d_slider){
         monitor_3d_slider = CONFIG_3D_SLIDERSTATE;
-        draw_mandelbrot_proper(false);
+        draw_mandelbrot_proper(m,true,false);
     }
     //draw_mandelbrot();
 
